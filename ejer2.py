da281741e18304b1c3a75d6e638714b39f8b74d2
TclaudiaC <claudia.torrejon@genstudents.org>
2024-11-26 15:38:20 +0000

    first commit

---
+++ ejer2.py
@@ -0,0 +1,14 @@
+mensaje = "Me llamo Claudia"
+print(mensaje)
+
+
+nombre = "claudia"
+print(f"hola , {nombre}. Bienvenido a Generetion.")
+
+
+nombre = input("introduce tu nombre: ")
+apellido = input("introduce tu apellido: ")
+print(f"hola , {nombre} {apellido}. Bienvenido a Generetion.")
+
+nombrecompleto = f"{nombre} {apellido}"
+print(nombrecompleto)
---
+++ ejer3.py
@@ -0,0 +1,5 @@
+nombre = input("introduce tu nombre: ")
+apellido = input("introduce tu apellido: ")
+edad = input("introduce tu edad: ")
+ciudad = input("introduce tu ciudad: ")
+print(f"hola , {nombre}  {apellido} {edad} años de la ciudad de {ciudad} . Bienvenido a Generetion.")
