1dc068a0c82db8553f18ba63728b21ba5b54943a
TclaudiaC <claudia.torrejon@genstudents.org>
2024-11-26 16:02:57 +0000

    nuevo cambio

--- ejer3.py
+++ ejer3.py
@@ -3,3 +3,11 @@ apellido = input("introduce tu apellido: ")
 edad = input("introduce tu edad: ")
 ciudad = input("introduce tu ciudad: ")
 print(f"hola , {nombre}  {apellido} {edad} años de la ciudad de {ciudad} . Bienvenido a Generetion.")
+
+#valor de su paga 
+
+horas=float(input("ingresa tus horas de trabajo: "))
+valor=float(input("ingresa el valor de tu hora: "))
+
+sueldo=horas*valor
+print(f"tu sueldo es , {sueldo} te felicito no faltes al trabajo ")
